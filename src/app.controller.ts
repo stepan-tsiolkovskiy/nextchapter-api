2d8a2baffd3a3d4575e1d798adc1442ee98d8fc4
stepan-tsiolkovskiy <tsiolkovskiystepan@gmail.com>
2025-06-28 23:04:06 +0300

    chore: remove default app service and test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRoot(): string {
+    return process.env.NODE_ENV !== 'production'
+      ? 'Welcome to the API. Docs available at /api'
+      : 'Welcome to the API.';
   }
 }
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello from Dock123112312sdfhsdhfkj!';
-  }
-}
